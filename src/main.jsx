c11493727b30281e62b0b9e574e04d96b7aec283
Luís Fernando Dandolini Duarte <luisfernandodandoliniduarte@gmail.com>
2023-07-18 18:53:29 -0300

    new font-family

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
+import './assets/fontes/GandhiSans-Regular.otf';
+import './assets/fontes/GandhiSans-Bold.otf';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
